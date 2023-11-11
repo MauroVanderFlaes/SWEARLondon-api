05c06f8fa916b38c6de8fb4fb411c778db615047
TiboMertens <tibomertens25@gmail.com>
2023-11-11 17:33:16 +0100

    feat: Add Order model schema

---
+++ models/Order.js
@@ -0,0 +1,20 @@
+//require mongoose
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+//create a schema for shoe orders, all fields are required except material, laces & sole
+const OrderSchema = new Schema({
+  brand: { type: String, required: true },
+  size: { type: Number, required: true },
+  material: { type: String, required: false },
+  laces: { type: String, required: false },
+  sole: { type: String, required: false },
+  color: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+  user: { type: String, required: true },
+});
+
+//export the model
+module.exports = mongoose.model("Shoe", ShoeSchema);
