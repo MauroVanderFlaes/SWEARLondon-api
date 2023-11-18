d5dee133826c579a8ab50a18528e20bd0380db4a
TiboMertens <tibomertens25@gmail.com>
2023-11-18 16:42:55 +0100

    feat: shoes can be updated

--- controllers/api/v1/shoes.js
+++ controllers/api/v1/shoes.js
@@ -91,7 +91,110 @@ const create = async (req, res) => {
   }
 };
 
+const update = async (req, res) => {
+  // Get the shoe id from the request parameters
+  let id = req.params.id; // Assuming the id is part of the URL parameters
 
+  // Get brand, size, laces, sole_bottom, sole_top, inside, outside_1, outside_2, outside_3, price, quantity, user from the request body
+  let {
+    brand,
+    size,
+    laces,
+    sole_bottom,
+    sole_top,
+    inside,
+    outside_1,
+    outside_2,
+    outside_3,
+    price,
+    quantity,
+    user,
+  } = req.body;
+
+  // Input validation
+  if (
+    !brand ||
+    !size ||
+    !laces ||
+    !sole_bottom ||
+    !sole_top ||
+    !inside ||
+    !outside_1 ||
+    !outside_2 ||
+    !outside_3 ||
+    !price ||
+    !quantity ||
+    !user
+  ) {
+    return res.status(400).json({
+      status: "error",
+      message: "Missing required fields",
+    });
+  }
+
+  try {
+    // Attempt to update the shoe
+    const result = await Shoe.updateOne(
+      { _id: id },
+      {
+        $set: {
+          brand,
+          size,
+          laces,
+          sole_bottom,
+          sole_top,
+          inside,
+          outside_1,
+          outside_2,
+          outside_3,
+          price,
+          quantity,
+          user,
+        },
+      }
+    );
+
+    // Check if the document was found and updated
+    if (result.matchedCount === 1 && result.modifiedCount === 1) {
+      // Respond with success
+      res.json({
+        status: "success",
+        message: "Shoe updated successfully!",
+        data: [
+          {
+            brand,
+            size,
+            laces,
+            sole_bottom,
+            sole_top,
+            inside,
+            outside_1,
+            outside_2,
+            outside_3,
+            price,
+            quantity,
+            user,
+          },
+        ],
+      });
+    } else {
+      // Respond with an error if the document was not found
+      res.status(404).json({
+        status: "error",
+        message: "Shoe not found",
+      });
+    }
+  } catch (error) {
+    // Handle the error
+    console.error("Error updating shoe:", error);
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
 
 //export the create function
 module.exports.create = create;
+module.exports.update = update;
