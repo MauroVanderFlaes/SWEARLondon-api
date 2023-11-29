bca53f0373233f057a8cf12ad10b9dac8e5c1e0c
TiboMertens <tibomertens25@gmail.com>
2023-11-29 18:29:42 +0100

    fix: Update admin default value in UserSchema

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ const UserSchema = new Schema({
   username: { type: String, required: true },
   user_mail: { type: String, required: true },
   password: { type: String, required: true },
-  admin: { type: Boolean, default: true },
+  admin: { type: Boolean, default: false },
 });
 
 //export the model
